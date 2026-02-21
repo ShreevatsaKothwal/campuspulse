59e7d6dce2895bcca414141238c98968de06483e
ShreevatsaKothwal <145187011+ShreevatsaKothwal@users.noreply.github.com>
2026-02-21 15:22:07 +0530

    Add local SQLite fallback

--- campuspulse/settings.py
+++ campuspulse/settings.py
@@ -95,7 +95,7 @@ WSGI_APPLICATION = 'campuspulse.wsgi.application'
 
 DATABASES = {
     'default': dj_database_url.config(
-        default=os.environ.get("DATABASE_URL")
+        default=f"sqlite:///{BASE_DIR / 'db.sqlite3'}"
     )
 }
 
